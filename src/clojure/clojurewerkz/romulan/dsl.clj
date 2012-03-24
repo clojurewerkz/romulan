ad4f9f790c7be3269b6987f21943fda3306ce8cd
Michael S. Klishin <michael@novemberain.com>
2012-03-24 05:54:36 +0400

    Initial commit

---
+++ src/clojure/clojurewerkz/romulan/dsl.clj
@@ -0,0 +1,94 @@
+(ns clojurewerkz.romulan.dsl
+  (:import [java.util.concurrent Executors ExecutorService]
+           [com.lmax.disruptor EventFactory EventTranslator EventHandler ClaimStrategy WaitStrategy]
+           [com.lmax.disruptor.dsl Disruptor]))
+
+;;
+;; Implementation
+;;
+
+(defn- fn->event-factory
+  "Creates a new Disruptor EventFactory that uses provided function"
+  [f]
+  (reify com.lmax.disruptor.EventFactory
+    (newInstance [this]
+      (f))))
+
+(defn- fn->event-handler
+  "Creates a new Disruptor EventHandler that uses provided function"
+  [f]
+  (reify com.lmax.disruptor.EventHandler
+    (onEvent [this event sequence end-of-batch?]
+      (f event sequence end-of-batch?))))
+
+(defn- fn->event-translator
+  "Creates a new Disruptor EventTranslator that uses provided function"
+  [f]
+  (reify com.lmax.disruptor.EventTranslator
+    (translateTo [this event sequence]
+      (f event sequence))))
+
+
+
+;;
+;; API
+;;
+
+(defprotocol EFFactory
+  (^com.lmax.disruptor.EventFactory event-factory [arg] "Creates Disruptor EventFactory from arg"))
+
+(extend-protocol EFFactory
+  EventFactory
+  (event-factory [^EventFactory arg]
+    arg)
+
+  clojure.lang.IFn
+  (event-factory [^clojure.lang.IFn arg]
+    (fn->event-factory arg)))
+
+
+(defprotocol EventHandlerFactory
+  (^com.lmax.disruptor.EventHandler event-handler [arg] "Creates an EventHandler from arg"))
+
+(extend-protocol EventHandlerFactory
+  EventHandler
+  (event-handler [arg]
+    arg)
+
+  clojure.lang.IFn
+  (event-handler [^clojure.lang.IFn arg]
+    (fn->event-handler arg)))
+
+
+(defprotocol EventTranslatorFactory
+  (^com.lmax.disruptor.EventTranslator event-translator [arg] "Creates an EventTranslator from arg"))
+
+(extend-protocol EventTranslatorFactory
+  EventTranslator
+  (event-translator [arg]
+    arg)
+
+  clojure.lang.IFn
+  (event-translator [^clojure.lang.IFn arg]
+    (fn->event-translator arg)))
+
+
+
+
+(defn ^com.lmax.disruptor.ExceptionHandler exception-handler
+  "Instantiates a new Disruptor exception handler that uses provided functions (:on-event, :on-start, :on-shutdown)"
+  [&{ :keys [on-event on-start on-shutdown] }]
+  (reify com.lmax.disruptor.ExceptionHandler
+    (handleEventException [this ex sequence event]
+      (on-event ex sequence event))
+    (handleOnStartException [this ex]
+      (on-start ex))
+    (handleOnShutdownException [this ex]
+      (on-shutdown ex))))
+
+
+(defn ^com.lmax.disruptor.dsl.Disruptor disruptor
+  ([ef ^long ring-buffer-size ^ExecutorService executor]
+     (Disruptor. (event-factory ef) ring-buffer-size executor))
+  ([ef ^ExecutorService executor ^ClaimStrategy cs ^WaitStrategy ws]
+     (Disruptor. (event-factory ef) executor cs ws)))
---
+++ test/clojurewerkz/romulan/dsl_test.clj
@@ -0,0 +1,75 @@
+(ns clojurewerkz.romulan.dsl-test
+  (:use clojure.test clojurewerkz.romulan.dsl)
+  (:import [java.util.concurrent Executors ExecutorService CountDownLatch]
+           [com.lmax.disruptor EventHandler EventTranslator ExceptionHandler SingleThreadedClaimStrategy BlockingWaitStrategy]
+           [com.lmax.disruptor.dsl Disruptor]))
+
+(deftest test-event-factory
+  (let [f  (constantly 42)
+        ef (event-factory f)]
+    (is (instance? com.lmax.disruptor.EventFactory ef))
+    (is (= 42 (.newInstance ef)))))
+
+(deftest test-handler-factory
+  (let [state (atom {})
+        fn    (fn [event sequence end-of-batch?]
+                (swap! state assoc :event event :sequence sequence :eob end-of-batch?))
+        eh      (event-handler fn)]
+    (is (instance? EventHandler eh))
+    (.onEvent eh { :type "dummy" } 50 true)
+    (is (= "dummy" (get-in @state [:event :type])))
+    (is (= 50      (:sequence @state)))
+    (is (:eob @state))))
+
+(deftest test-event-translator
+  (let [et (event-translator (fn [x _] x))]
+    (is (instance? com.lmax.disruptor.EventTranslator et))
+    (are [input output] (is (= output (.translateTo et input 0)))
+         1     1
+         "2"   "2"
+         3.0   3.0
+         4/5   4/5
+         'five 'five
+         :six  :six)))
+
+(deftest test-exception-handler-factory
+  (letfn [(event-ehf    [ex l evt])
+          (startup-ehf  [ex])
+          (shutdown-ehf [ex])]
+    (is (instance? ExceptionHandler (exception-handler :on-event event-ehf
+                                                       :on-start startup-ehf
+                                                       :on-shutdown shutdown-ehf)))))
+
+(deftest test-basic-dsl-example1
+  (let [^Disruptor d (disruptor (event-factory (constantly 99))
+                                (Executors/newFixedThreadPool 2)
+                                (SingleThreadedClaimStrategy. 4)
+                                (BlockingWaitStrategy.))]
+    (.start d)
+    (.shutdown d)))
+
+(deftest test-basic-dsl-example2
+  (let [^Disruptor d (disruptor (constantly 99)
+                                256
+                                (Executors/newFixedThreadPool 2))]
+    (.start d)
+    (.shutdown d)))
+
+
+(deftest test-publishing-and-handling-events-using-java-interop-example1
+  (let [n     87
+        latch (CountDownLatch. n)
+        eh    (event-handler (fn [event sequence end-of-batch?]
+                               ))
+        et    (event-translator (fn [x _]
+                                  (.countDown latch)
+                                  x))
+        ^Disruptor d  (disruptor (constantly 99)
+                                 256
+                                 (Executors/newFixedThreadPool 2))]
+    (.handleEventsWith d (into-array EventHandler [eh]))
+    (.start d)
+    (dotimes [i n]
+      (.publishEvent d et))
+    (.await latch)
+    (.shutdown d)))
